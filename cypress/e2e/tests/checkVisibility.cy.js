139ffb2bf773f10049f8a2711a06f704558199b3
alphatypes1 <alphanozbusiness@outlook.com>
2023-08-25 15:43:03 +0300

    added two more assertions to the test checkVisibility

--- cypress/e2e/tests/checkVisibility.cy.js
+++ cypress/e2e/tests/checkVisibility.cy.js
@@ -2,5 +2,8 @@ describe('Checkbox', () => {
   it('Checking and asserting checkboxes', () => {
     cy.visit('http://zero.webappsecurity.com');
     cy.get('#signin_button').should('be.visible');
+
+    cy.get('#searchTerm').should('be.visible');
+    cy.get('#homeMenu').should('be.visible');
   });
 });
